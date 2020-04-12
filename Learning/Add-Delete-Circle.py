03057b539c3ec351e28c5b6f47771940d1960bb9
Darrel Ronald <DarrelRonald@users.noreply.github.com>
2020-04-12 10:19:26 +0200

    Update Add-Delete-Circle.py

--- Learning/Add-Delete-Circle.py
+++ Learning/Add-Delete-Circle.py
@@ -6,7 +6,8 @@ import scriptcontext as sc
 import System.Guid
 import rhinoscriptsyntax as rs
 
-def AddCircle():  # Written by McNeel
+# addCircle written by McNeel
+def AddCircle():  
     center = Rhino.Geometry.Point3d(0, 0, 0)
     radius = 10.0
     c = Rhino.Geometry.Circle(center, radius)
@@ -16,7 +17,8 @@ def AddCircle():  # Written by McNeel
         return Rhino.Commands.Result.Success
     return Rhino.Commands.Result.Failure
 
-def AddCircle2():  # Written by Darrel Ronald
+ # addCircle rewritten to see what is necessary
+def AddCircle2(): 
     center = Rhino.Geometry.Point3d(5, 5, 5)
     radius = 20.0
     c = Rhino.Geometry.Circle(center, radius)
@@ -27,15 +29,35 @@ def AddCircle2():  # Written by Darrel Ronald
 
 if __name__=="__main__":
 
+    # Add geometry to delete
     AddCircle2()
-    #----Option Rhinoscriptsyntax
+    
+    #----Option "Delete" using Rhinoscriptsyntax
     id = rs.AllObjects(select=True)
     rs.DeleteObjects(id)
     
+    # Add geometry to delete
     AddCircle()
     
-    #---- Option Scriptcontext
+    #---- Option "Delete" using Scriptcontext
     sc.doc.Objects.Clear()
     
+    # Add geometry to delete
+    AddCircle()
     AddCircle2()
     
+
+    #--- Option "Delete" using RhinoCommon
+    ot = rs.AllObjects(select=True) #Rhino.RhinoDoc.ActiveDoc.Objects
+    ot_id = []
+    
+    def idMaker(objects):
+        for i in ot:
+           Rhino.DocObjects.RhinoObject.Id
+           ot_id.append(i)
+    idMaker(ot)
+    Rhino.RhinoDoc.ActiveDoc.Objects.Delete(ot_id, True)
+
+
+    # Add geometry to delete
+    AddCircle2()
